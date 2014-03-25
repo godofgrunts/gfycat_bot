0bb6cf2f66a34b053ea36dea338b6c8c518963ed
Ryan Whited <r.a.whited@gmail.com>
2014-03-25 03:09:30 -0400

    Fixed pickle
    Fixed pickle.load to 'rb'
    Fixed pickle.dump to 'wb'
    
    Turns out, python set variables top down as it compiles and not by
    function call (which is different from C++) so I had to move pickle.load
    to the top and set already_done as a global in postGfy because python
    doesn't like nested variables...

--- gfycat_bot.py
+++ gfycat_bot.py
@@ -7,7 +7,7 @@ import datetime
 import pickle
 import praw #Python Reddit API Wrapper https://github.com/praw-dev/praw
 
-user_agent = ("gif_2_gfy_bot 2.1 by /u/lol_gog")
+user_agent = ("gif_2_gfy_bot 2.5 by /u/lol_gog")
 
 data_file = 'already.dat'
 
@@ -16,6 +16,13 @@ r.login('USERNAME','PASSWORD') #CHANGE THIS FOR YOUR BOT
 print('Logged in')
 
 already_done = [] #this hold the submission ids for all the post that are done.
+try:
+	with open(data_file, 'rb') as f:
+		already_done = pickle.load(f) #load already_done
+		f.close()
+except IOError as e:
+	pass # it's ok if it doesn't exist
+
 
 gif =['.gif']
 newGfy='http://gfycat.com/'
@@ -32,29 +39,30 @@ def urlCreator(x):
 
 def postGfy(submissions):
 
+	global already_done
 	is_gif = any(string in submissions.url for string in gif)
 	if submissions.id not in already_done and is_gif:
-			msg = '[GIF FOUND]--->%s' % submissions.short_link
-			print(msg) #Going to leave this one up.
-			ts = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
-			print(ts)
-			postUrl=submissions.short_link
-			gifUrl=submissions.url
-			print("Sleeping for 30 seconds to please the gfycat gods...")
-			time.sleep(30)
-			gfyUrl=urlCreator(gifUrl)
-			j = urllib.request.urlopen(gfyUrl).read()
-			jstr = str(j)
-			array = jstr.split("\"")
-			newUrl = newGfy+array[3]
-			try:
-				r.submit('SUBREDDIT YOU ARE SUBMITTING TO', submissions.id, url=newUrl)
-			except praw.errors.AlreadySubmitted:
-				print("URL already submitted... Moving on.")
-			already_done.append(submissions.id)
-			outfile = open(data_file ,"wb")
-			pickle.dump(already_done, outfile) #dump already_done to file
-			outfile.close()
+		msg = '[GIF FOUND]--->%s' % submissions.short_link
+		print(msg) #Going to leave this one up.
+		ts = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+		print(ts)
+		postUrl=submissions.short_link
+		gifUrl=submissions.url
+		print("Sleeping for 30 seconds to please the gfycat gods...")
+		time.sleep(30)
+		gfyUrl=urlCreator(gifUrl)
+		j = urllib.request.urlopen(gfyUrl).read()
+		jstr = str(j)
+		array = jstr.split("\"")
+		newUrl = newGfy+array[3]
+		try:
+			r.submit('SUBREDDIT YOU ARE SUBMITTING TO', submissions.id, url=newUrl)
+		except praw.errors.AlreadySubmitted:
+			print("URL already submitted... Moving on.")
+		already_done.append(submissions.id)
+		outfile = open(data_file, 'wb')
+		pickle.dump(already_done, outfile) #dump already_done to file
+		outfile.close()
 
 def searchSubreddit(subs): #reddit[y]
 	sub = r.get_subreddit(subs)
@@ -67,12 +75,6 @@ def searchSubreddit(subs): #reddit[y]
 
 
 def main():
-	try:
-		with open(data_file, 'at', encoding='utf8') as f:
-			already_done = pickle.load(f) #load already_done
-			f.close()
-	except IOError as e:
-		pass # it's ok if it doesn't exist
 	while True:
 		for reddit in reddits:
 			searchSubreddit(reddit)
